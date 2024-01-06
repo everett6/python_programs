9d692d291e5edf49d8c9942bc0c9274aae6c55b6
Xiufeng Song <xiufeng.song@gmail.com>
2024-01-06 14:25:18 -0800

    geomery commit

---
+++ Area_Calculator.py
@@ -0,0 +1,81 @@
+import math,time
+
+def area_of_square(side: float):
+    area = pow(side,2)
+    return area
+
+def area_of_rectangle(length: float, width: float):
+    area= length*width
+    return area
+
+def area_of_circle(radius: float):
+    area= pow(radius,2) * 3.141592
+    return area
+ 
+def area_of_pentagon(side: float):
+    area= 0.25 * math.sqrt(5*(5+2*math.sqrt(5)))*pow(side,2)
+    return area
+
+def area_of_hexagon(side: float):
+    area= 1.5*math.sqrt(3) * pow(side,2)
+    return area
+
+# --- main function entry ----------------- 
+start_time=time.time()
+while True: 
+    question= int(input("how much times would you like to do this: "))
+    if question>0:
+        break
+    elif question==0:
+        quit()
+    elif time.time()-start_time>30:
+        print("Sorry you ran out of time")
+        exit()
+
+shape_names=["square","circle","hexagon","pentagon","rectangle"]
+
+def validation(shape,shape_names):
+    for s in shape_names:
+        if s==shape:
+            return True
+    return shape=="quit"
+    
+
+starttime=time.time()
+for i in range(question):
+    
+    print("We only support: ", shape_names)
+    shape=input("what is the shape you want to compute: ").lower()
+    if time.time()-starttime>15:
+        print("you ran out of time")
+        exit()
+    if validation(shape,shape_names)==False:
+        continue
+    starttime=time.time()
+
+    if shape=="quit":
+        break
+    elif shape =="rectangle":
+        length=input("what is the length: ")
+        width= input("what is the width: ")
+        length=float(length)
+        width=float(width)
+        area= area_of_rectangle(length,width)
+        print(area)
+    elif shape =="circle":
+        radius =input("what is your radius: ")
+        radius=float(radius)
+        area= area_of_circle(radius)
+        print(area)
+    else:
+        side=input("what is the side: ")
+        side=float(side)
+        if shape =="square":
+            area=area_of_square(side)
+            print(area)
+        elif shape =="pentagon":
+            area=area_of_pentagon(side)
+            print(area)
+        elif shape =="hexagon":
+            area=area_of_hexagon(side)
+            print(area)
---
+++ Equation_Solver.py
@@ -0,0 +1,45 @@
+import math,time
+
+def quadratic_function_solver(a:float,b:float,c: float)-> list:
+    if a==0 and b==0 and c==0:
+        return ["Infinite Solutions"]
+    if b==0 and a==0:
+        return []
+    if a ==0:
+        x=-c/b
+        return x 
+
+    delta= pow(b,2) - 4*a*c
+    if delta==0:
+        x=-b/(2*a)
+        return x
+    elif delta <0:
+        x=-b/(2*a) + (1j)*math.sqrt(-delta)/(2*a)
+        x2=x.conjugate()
+        return x,x2
+    else:
+        
+        x1= (-b+ math.sqrt(delta)) /(2*a)
+        x2= (-b-math.sqrt(delta))/(2*a)
+        return x1,x2
+    
+# --- main function entry ----------------- 
+start_time=time.time()
+if time.time()-start_time>15:
+    print("you ran out of time")
+    exit()
+question=int(input("How much times would you like to do this: "))
+if time.time()-start_time>10:
+    print("You ran out of time")
+    exit()
+for i in range(question):
+    print("We only support quadratic equations in the form ax**2 + bx + c")
+    a=float(input("What is your a: "))
+    b=float(input("what is your b: "))
+    c=float(input("What is your c: "))
+    
+    answer=quadratic_function_solver(a,b,c)
+    print("The answer is", answer)
+
+
+    
